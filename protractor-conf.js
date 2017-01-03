2b28c370eb68afa5325c87ebef160533f2ba6207
Marcel Mokoš <marcel.mokos@gmail.com>
2017-01-03 11:45:28 +0100

    jasmine2 support

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,12 +1,14 @@
 //noinspection JSUnusedGlobalSymbols
+const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+
 exports.config = {
-  framework: "jasmine",
+  framework: "jasmine2",
   specs: ["spec/**/*.spec.js"],
   directConnect: true,
   chromeDriver: "node_modules/chromedriver/lib/chromedriver/chromedriver",
   jasmineNodeOpts: {
     // remove ugly protractor dot reporter
-    print: function () {}
+    print: function () {},
   },
   onPrepare: function () {
     require("babel-core/register");
@@ -24,24 +26,22 @@ exports.config = {
      */
     browser.ignoreSynchronization = true;
 
-    const env = jasmine.getEnv();
-    const SpecReporter = require("jasmine-spec-reporter");
-
-    env.addReporter(
+    jasmine.getEnv().addReporter(
       new SpecReporter({
+        spec: {
+          // Defaults: https://github.com/bcaudan/jasmine-spec-reporter#default-options
+          displaySuccessesSummary: false, // display summary of all successes after execution
+          displayFailuresSummary: false,  // display summary of all failures after execution
+          displayPendingSummary: false,   // display summary of all pending specs after execution
 
-        // Defaults: https://github.com/bcaudan/jasmine-spec-reporter#default-options
-        displaySuccessesSummary: false, // display summary of all successes after execution
-        displayFailuresSummary: false,  // display summary of all failures after execution
-        displayPendingSummary: false,   // display summary of all pending specs after execution
-
-        displayPendingSpec: true,       // display each pending spec
-        displaySpecDuration: true,      // display each spec duration
-        displaySuiteNumber: true,       // display each suite number (hierarchical)
+          displayPendingSpec: true,       // display each pending spec
+          displaySpecDuration: true,      // display each spec duration
+          displaySuiteNumber: true,       // display each suite number (hierarchical)
+        },
       })
     );
   },
   capabilities: {
-    browserName: "chrome"
+    browserName: "chrome",
   },
 };
